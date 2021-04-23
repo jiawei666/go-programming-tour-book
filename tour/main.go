3092ddc5db70b3d6131c5424bc65a678fc62d9e1
jiawei666 <957089263@qq.com>
2021-04-23 18:56:03 +0800

    init

---
+++ tour/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	var name string
+	flag.StringVar(&name, "name", "Go语言编程之旅", "帮助信息")
+	flag.StringVar(&name, "n", "Go语言编程之旅", "帮助信息")
+	flag.Parse()
+
+	//return
+	log.Printf("name:%s", name)
+}
